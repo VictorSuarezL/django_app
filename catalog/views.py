658d7dc785565739940e6702206edd87772bd36f
Victor Suarez-Lledo [vsanz-su] <33570867+VictorSuarezL@users.noreply.github.com>
2024-05-18 19:33:21 +0200

    First view ready in example mode

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def catalog_list(request):
+    # return HttpResponse("Your are here!")
+    return render(request, 'catalog/catalog_list.html', {})
