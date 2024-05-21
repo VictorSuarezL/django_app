df74d33df6819b024964c96219002288c39cdd79
Victor Suarez-Lledo [vsanz-su] <33570867+VictorSuarezL@users.noreply.github.com>
2024-05-21 21:13:46 +0200

    feat: Extract unique values from dataframe and export to JSON
    This commit adds a new script `eda.py` that performs exploratory data analysis on the `Datos.xlsx` file. The script reads the Excel file, extracts unique values from the 'Tipo de IVA', 'Servicio anterior', and 'Garantía' columns, and exports them to separate JSON files. This functionality will help in analyzing and processing the data more efficiently.

--- data/eda.py
+++ data/eda.py
@@ -1,33 +1,97 @@
+# from catalog.models import Car, Brand
 import pandas as pd
 import json
 
-df = pd.read_excel("./data/Datos.xlsx", sheet_name="Export_datos")
+# from catalog.models import Brand
 
-# df
 
-# # Get unique values for Marca variable in df without nan values
-# unique_values = df['Tipo de IVA'].unique()
-# unique_values = unique_values[~pd.isnull(unique_values)]
+# def transform_data(df, col_to_read, name_json):
+#     unique_values = df[col_to_read].unique()
+#     unique_values = unique_values[~pd.isnull(unique_values)]
+#     unique_values = unique_values.tolist()
+#     print(unique_values)
+#     with open(f'data/{name_json}.json', 'w') as f:
+#         json.dump(unique_values, f)
 
-# print(unique_values)
+# transform_data(df, 'Tipo de IVA', 'iva')
+# transform_data(df, 'Servicio anterior', 'servicio_anterior')
+# transform_data(df, 'Garantía', 'garantia')
 
-# # Export unique values to a json file with name iva.json
-# unique_values = unique_values.tolist()
-# with open('data/iva.json', 'w') as f:
-#     json.dump(unique_values, f)
+
+# ------ Load data from excel to database ------
+
+df = pd.read_excel("./data/Datos.xlsx", sheet_name="Export_datos").drop_duplicates().iloc[0:10, :]
+print(df.shape)
+
+brands = json.load(open("data/brands_models_cleaned.json"))
+
+brand_list = []
+for brand in brands:
+    brand_list.append(brand)
+    # Brand.objects.get_or_create(name=brand)
+    # print(brand + ' creado')
+
+print(brand_list)
+
+df = df[df["Marca"].isin(brand_list)]
+
+# print(df.shape)
+# print(df.columns.to_list())
+
+# # Rename columns
+df.rename(
+    columns={
+        "Matrícula": "matricula",
+        "Chasis": "chassis",
+        "Fecha matriculación": "registration_date",
+        # "Documentado": "documented",
+        "Marca": "brand",
+        "Color": "color",
+        "Precio contado": "buy_price",
+        "Precio financiado": "sale_price",
+        "Kilómetros": "km",
+        "Combustible": "fuel",
+        "Cambio": "transmission",
+        "Versión": "version",
+        "Modelo": "car_model",
+    },
+    inplace=True,
+)
+
+# print(df.columns.to_list())
+
+# Select columns
+df = df.loc[:, ["matricula", "chassis", "registration_date", "brand", "color", "buy_price", "sale_price", "km", "fuel", "transmission", "version", "car_model"]]
+
+# Create new column documented with default value True
+df["documented"] = True
+
+# Filter only complete rows
+df = df.dropna()
+print(df.shape)
+
+# Send data to database
+
+# for index, row in df.iterrows():
+#     print(index)
+
+# for index, row in df.iterrows():
+#     brand = row["brand"]
+#     brand = Brand.objects.get(name=brand)
+#     Car.objects.get_or_create(
+#         matricula=row["matricula"],
+#         chassis=row["chassis"],
+#         registration_date=row["registration_date"],
+#         documented=row["documented"],
+#         brand=brand,
+#         color=row["color"],
+#         buy_price=row["buy_price"],
+#         sale_price=row["sale_price"],
+#         km=row["km"],
+#         fuel=row["fuel"],
+#         transmission=row["transmission"],
+#         version=row["version"],
+#         car_model=row["car_model"],
+#     )
     
-
-def transform_data(df, col_to_read, name_json):
-    unique_values = df[col_to_read].unique()
-    unique_values = unique_values[~pd.isnull(unique_values)]
-    unique_values = unique_values.tolist()
-    print(unique_values)
-    with open(f'data/{name_json}.json', 'w') as f:
-        json.dump(unique_values, f)
-
-transform_data(df, 'Tipo de IVA', 'iva')
-transform_data(df, 'Servicio anterior', 'servicio_anterior')
-transform_data(df, 'Garantía', 'garantia')
-
-
-df
+    
--- data/load_data.py
+++ data/load_data.py
@@ -1,5 +1,8 @@
+# Se ejecuta con el comando: python manage.py shell < data/load_data.py
+
 import json
-from catalog.models import Brand 
+from catalog.models import Brand, Car 
+import pandas as pd
 
 # with open('data/brands_models.json', 'r') as file:
 #     brands = json.load(file)
@@ -16,3 +19,52 @@ def load_data_json(json_name, model):
         model.objects.get_or_create(name=item)
         print(item + ' creado')
 
+    return data
+
+brands = load_data_json('brands_models_cleaned', Brand)
+
+df = pd.read_excel("./data/Datos.xlsx", sheet_name="Export_datos").drop_duplicates().iloc[0:10, :]
+
+df.rename(
+    columns={
+        "Matrícula": "matricula",
+        "Chasis": "chassis",
+        "Fecha matriculación": "registration_date",
+        # "Documentado": "documented",
+        "Marca": "brand",
+        "Color": "color",
+        "Precio contado": "buy_price",
+        "Precio financiado": "sale_price",
+        "Kilómetros": "km",
+        "Combustible": "fuel",
+        "Cambio": "transmission",
+        "Versión": "version",
+        "Modelo": "car_model",
+    },
+    inplace=True,
+)
+
+df = df.loc[:, ["matricula", "chassis", "registration_date", "brand", "color", "buy_price", "sale_price", "km", "fuel", "transmission", "version", "car_model"]]
+df["documented"] = True
+df = df.dropna()
+
+for index, row in df.iterrows():
+    print(index)
+    if row["brand"] in brands:
+        brand = row["brand"]
+        brand = Brand.objects.get(name=brand)
+        Car.objects.get_or_create(
+            matricula=row["matricula"],
+            chassis=row["chassis"],
+            registration_date=row["registration_date"],
+            documented=row["documented"],
+            brand=brand,
+            color=row["color"],
+            buy_price=row["buy_price"],
+            sale_price=row["sale_price"],
+            km=row["km"],
+            fuel=row["fuel"],
+            transmission=row["transmission"],
+            version=row["version"],
+            car_model=row["car_model"],
+        )
