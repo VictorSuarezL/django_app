c4604bd047a17640c706b77084f48e4c480c7e16
Victor Suarez-Lledo [vsanz-su] <33570867+VictorSuarezL@users.noreply.github.com>
2024-06-08 19:59:44 +0200

    feat: Update load_data.py to load data from new Excel file and create Car objects
    This commit updates the load_data.py script to load data from a new Excel file named "Principal.xlsx". It also modifies the code to create Car objects using the data from the Excel file. The previous code was limited to loading a subset of data and had some unused fields commented out. This update expands the functionality to load all columns and creates Car objects with the appropriate fields. The commit also includes error handling for missing brands and saves any erroneous data to a separate Excel file for further analysis.

--- data/eda.py
+++ data/eda.py
@@ -1,9 +1,49 @@
 # from catalog.models import Car, Brand
+from enum import unique
 import pandas as pd
 import json
 
 # from catalog.models import Brand
 
+# ------ Load data from excel to database ------
+
+def date_parser(x):
+    return pd.to_datetime(x, format="%d/%m/%y").date()
+
+
+# Carga los datos del archivo Excel
+df = pd.read_excel(
+    "./data/Principal.xlsx",
+    sheet_name="Principal",
+    parse_dates=True,
+    date_format=date_parser,
+    engine='openpyxl'
+)
+
+with open("data/json/column_dict.json", "r") as f:
+    nuevos_nombres = json.load(f)
+
+df.rename(columns=nuevos_nombres, inplace=True)
+
+# df = df.drop(df.index[5469])
+
+json_dict = {}
+for column in ["provincia", "localidad", "vendedor"]:
+    df[column] = df[column].astype(str)
+    unique_values = sorted(df[column].unique().tolist())
+    json_dict[column] = unique_values
+
+json_str = json.dumps(json_dict, ensure_ascii=False, indent=4)
+
+print(json_str)
+
+with open("data/json/unique_values.json", "w", encoding='utf-8') as f:
+    f.write(json_str)
+    
+
+
+
+# #------ Transform data to json ------
 
 # def transform_data(df, col_to_read, name_json):
 #     unique_values = df[col_to_read].unique()
@@ -13,114 +53,37 @@ import json
 #     with open(f'data/{name_json}.json', 'w') as f:
 #         json.dump(unique_values, f)
 
-# transform_data(df, 'Tipo de IVA', 'iva')
-# transform_data(df, 'Servicio anterior', 'servicio_anterior')
-# transform_data(df, 'Garantía', 'garantia')
+# transform_data(df, 't_iva', 't_iva')
+# transform_data(df, 'servi_anterior', 'servi_anterior')
+# transform_data(df, 'garantia', 'garantia')
+# transform_data(df, 'provincia', 'provincia')
 
 
-# ------ Load data from excel to database ------
-
-# df = pd.read_excel("./data/Datos.xlsx", sheet_name="Export_datos").drop_duplicates().iloc[0:10, :]
-df = pd.read_excel("./data/Datos.xlsx", sheet_name="Export_datos").drop_duplicates()
-
-# df.drop(5469, inplace=True)
-
-df = df.drop(df.index[5469])
-
-print(df)
-
-marcas = json.load(open("data/marcas_models_cleaned.json"))
-
-marca_list = []
-for marca in marcas:
-    marca_list.append(marca)
-    # Brand.objects.get_or_create(name=marca)
-    # print(marca + ' creado')
-
-# print(marca_list)
-
-# Rename columns
-df.rename(
-    columns={
-        "Matrícula": "matricula",
-        "Chasis": "chasis",
-        "Fecha matriculación": "f_matriculacion",
-        # "Documentado": "documented",
-        "Marca": "marca",
-        "Color": "color",
-        "Precio contado": "buy_price",
-        "Precio financiado": "sale_price",
-        "Kilómetros": "km",
-        "Combustible": "combustible",
-        "Cambio": "cambio",
-        "Versión": "version",
-        "Modelo": "modelo",
-    },
-    inplace=True,
-)
-
-#------ Filter wrong data ------
-
-# Filter only rows without marca in marca list
-
-# df = df[~df["marca"].isin(marca_list)]
-
-# Change the value where marca is equal to "Infiniti" to "Nissan"
-df.loc[df["marca"] == "Bmw", "marca"] = "BMW"
-df.loc[df["marca"] == "Toyoya", "marca"] = "Toyota"
-df.loc[df["marca"].isin(["Citroen", "Citröen"]) , "marca"] = "Citroën"
-df.loc[(df["marca"].isna()) & (df["modelo"] == "Q30"), "marca"] = "Infiniti"
-df.loc[(df["marca"].isna()) & (df["modelo"] == "Clio"), "marca"] = "Renault"
-
-# df = df[~df["marca"].isin(marca_list)]
-
-# print(df.loc[df["marca"].isna()])
-
-# print(df["marca"].unique().tolist())
-
-""" 
-#------ Filter data ------
-df = df[df["marca"].isin(marca_list)]
-
-# print(df.shape)
-# print(df.columns.to_list())
 
 
-# print(df.columns.to_list())
+# #------ Load data from json to database ------
+# marcas = json.load(open("data/json/brands_models_cleaned.json"))
 
-# Select columns
-df = df.loc[:, ["matricula", "chasis", "f_matriculacion", "marca", "color", "buy_price", "sale_price", "km", "combustible", "cambio", "version", "modelo"]]
+# marca_list = []
+# for marca in marcas:
+#     marca_list.append(marca)
 
-# Create new column documented with default value True
-df["documented"] = True
+# #------ Filter wrong data ------
 
-# Filter only complete rows
-df = df.dropna()
-print(df.shape)
+# # Filter only rows without marca in marca list
 
-# Send data to database
+# # df = df[~df["marca"].isin(marca_list)]
 
-# for index, row in df.iterrows():
-#     print(index)
+# # Change the value where marca is equal to "Infiniti" to "Nissan"
+# df.loc[df["marca"] == "Bmw", "marca"] = "BMW"
+# df.loc[df["marca"] == "Toyoya", "marca"] = "Toyota"
+# df.loc[df["marca"].isin(["Citroen", "Citröen"]) , "marca"] = "Citroën"
+# df.loc[(df["marca"].isna()) & (df["modelo"] == "Q30"), "marca"] = "Infiniti"
+# df.loc[(df["marca"].isna()) & (df["modelo"] == "Clio"), "marca"] = "Renault"
 
-# for index, row in df.iterrows():
-#     marca = row["marca"]
-#     marca = Brand.objects.get(name=marca)
-#     Car.objects.get_or_create(
-#         matricula=row["matricula"],
-#         chasis=row["chasis"],
-#         f_matriculacion=row["f_matriculacion"],
-#         documented=row["documented"],
-#         marca=marca,
-#         color=row["color"],
-#         buy_price=row["buy_price"],
-#         sale_price=row["sale_price"],
-#         km=row["km"],
-#         combustible=row["combustible"],
-#         cambio=row["cambio"],
-#         version=row["version"],
-#         modelo=row["modelo"],
-#     )
-    
-    
- """
+# # df = df[~df["marca"].isin(marca_list)]
+
+# # print(df.loc[df["marca"].isna()])
+
+# # print(df["marca"].unique().tolist())
+
--- data/load_data.py
+++ data/load_data.py
@@ -1,75 +1,77 @@
 # Se ejecuta con el comando: python manage.py shell < data/load_data.py
 
 import json
-from catalog.models import Brand, Car 
 import pandas as pd
+from catalog.models import Brand, Car
+from sales.models import Province, Municipality, Saler, Client, Sales
 
-# with open('data/marcas_models.json', 'r') as file:
+
+# with open('data/brands_models_cleaned.json', 'r') as file:
 #     marcas = json.load(file)
 
 # for marca_name in marcas:
 #     Brand.objects.get_or_create(name=marca_name)
 #     print(marca_name + ' creado')
-    
+
+
 def load_data_json(json_name, model):
-    with open(f'data/{json_name}.json', 'r') as file:
+    with open(f"data/json/{json_name}.json", "r") as file:
         data = json.load(file)
-    
+
     for item in data:
         model.objects.get_or_create(name=item)
-        print(item + ' creado')
+        print(item + " creado")
 
     return data
 
-marcas = load_data_json('brands_models_cleaned', Brand)
 
-df = pd.read_excel("./data/Datos.xlsx", sheet_name="Export_datos").drop_duplicates().iloc[0:10, :]
+marcas = load_data_json("brands_models", Brand)
 
-with open('column_dict.json', 'r') as f:
+
+def date_parser(x):
+    return pd.to_datetime(x, format="%d/%m/%y").date()
+
+
+# Carga los datos del archivo Excel
+df = pd.read_excel(
+    "./data/Principal.xlsx",
+    sheet_name="Principal",
+    parse_dates=True,
+    date_format=date_parser,
+)
+
+# Elimina duplicados y selecciona las primeras 10 filas
+df = df.drop_duplicates().iloc[0:10, :]
+
+with open("data/json/column_dict.json", "r") as f:
     nuevos_nombres = json.load(f)
 
 df.rename(columns=nuevos_nombres, inplace=True)
 
-""" df.rename(
-    columns={
-        "Matrícula": "matricula",
-        "Chasis": "chasis",
-        "Fecha matriculación": "f_matriculacion",
-        # "Documentado": "documented",
-        "Marca": "marca",
-        "Color": "color",
-        "Precio contado": "buy_price",
-        "Precio financiado": "sale_price",
-        "Kilómetros": "km",
-        "Combustible": "combustible",
-        "Cambio": "cambio",
-        "Versión": "version",
-        "Modelo": "modelo",
-    },
-    inplace=True,
-) """
+# df = df.loc[:, ["matricula", "chasis", "f_matriculacion", "marca", "color", "buy_price", "sale_price", "km", "combustible", "cambio", "version", "modelo"]]
+# df["documented"] = True
+# # df = df.dropna()
 
-df = df.loc[:, ["matricula", "chasis", "f_matriculacion", "marca", "color", "buy_price", "sale_price", "km", "combustible", "cambio", "version", "modelo"]]
-df["documented"] = True
-df = df.dropna()
+car_fields = [field.name for field in Car._meta.get_fields()]
+
+# Crear una dataframe vacio igual que df para almacenar los errores de la carga
+df_errores = pd.DataFrame(columns=df.columns)
 
 for index, row in df.iterrows():
-    print(index)
-    if row["marca"] in marcas:
-        marca = row["marca"]
-        marca = Brand.objects.get(name=marca)
-        Car.objects.get_or_create(
-            matricula=row["matricula"],
-            chasis=row["chasis"],
-            f_matriculacion=row["f_matriculacion"],
-            documented=row["documented"],
-            marca=marca,
-            color=row["color"],
-            buy_price=row["buy_price"],
-            sale_price=row["sale_price"],
-            km=row["km"],
-            combustible=row["combustible"],
-            cambio=row["cambio"],
-            version=row["version"],
-            modelo=row["modelo"],
-        )
+    car = Car()
+    for field in car_fields:
+        if field == "marca":
+            try:
+                brand = Brand.objects.get(name=row[field])
+            except Brand.DoesNotExist:
+                df_errores = df_errores.append(row)
+                print(f"Marca {row[field]} no encontrada")
+                continue
+            
+            setattr(car, field, brand)
+        else:
+            setattr(car, field, row[field])
+    car.save()
+    print(f"Coche {car.matricula} creado")
+
+df_errores.to_excel("data/errores.xlsx", index=False)
